90de273a5c5d4a8b7593a75ee1a8b9a38e436284
duonghonglam0805 <honglamhk2004@gmail.com>
2024-06-14 15:54:30 +0700

    Completed creating Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
-import pnLogo from "./assets/pn-logo.png";
-
+import Header from "./components/Header";
 function App() {
   return (
     <>
-      <header id="header">
-        <img src={pnLogo} alt="PN Logo" />
-        <h1>Students results for (fake batch name))</h1>
-      </header>
-
+    <Header />
       <main className="scores-container">
         <div class="scores">
-          <h1>Fake Course</h1>
-
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
+        
         </div>
       </main>
     </>
